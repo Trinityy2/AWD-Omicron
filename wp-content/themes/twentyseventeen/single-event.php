c9bb4fe6c319e965413e08bffadf7073d8cdf31b
Tomo <tomohito95@hotmail.com>
2018-11-02 01:45:05 +1100

    finished events single and single php files

--- wp-content/themes/twentyseventeen/single-event.php
+++ wp-content/themes/twentyseventeen/single-event.php
@@ -12,17 +12,20 @@
   <div class="wrap">
 
     <?php while ( have_posts() ) : the_post(); ?>
-      <h1> <?php echo wp_title(); ?> </h1>
 
       <div class="eventBox">
         <iframe class="video" width="560" height="315" src="<?php echo get_field("trailer_embed_link")?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> </iframe>
-        <p class="quickInfo">
-          <?php echo get_field("director"); ?> ||
-          <?php echo get_field("year_of_release"); ?> ||
-          <?php echo get_field("duration"); ?> min ||
-          <?php echo get_field("available_subtitles"); ?> ||
-          <?php echo get_field("genre"); ?>
-        </p>
+        <section class="quickInfo">
+          <p class="title"> <b><?php echo get_the_title(); ?> </b></p>
+
+          <p>
+            <?php echo get_field("director"); ?> ||
+            <?php echo get_field("year_of_release"); ?> ||
+            <?php echo get_field("duration"); ?> min ||
+            <?php echo get_field("available_subtitles"); ?> ||
+            <?php echo get_field("genre"); ?>
+          </p>
+        </section>
         <p class="restrictions">
           <?php echo get_field("restrictions"); ?>
         </p>
@@ -30,16 +33,16 @@
         <section class="description">
           <?php the_content(); ?>
         </section>
-        <p class="dateTime">
+        <p class="dateTimeLocation">
           <b>Date and Time: </b> <br>
           <?php $date = get_field('date', false, false);
           $date = new DateTime($date);
           echo $date->format('F d Y');
-           ?>, <?php echo get_field("time"); ?>
-        </p>
-        <p class="location">
+           ?>, <?php echo get_field("time"); ?> <br><br>
           <b>Location</b> <br>
-          <?php echo get_field("location");?> </p>
+          <?php echo get_field("location");?> <br>
+          <iframe src="http://maps.google.com/maps?output=embed&z=15&q=<?php echo get_field("location");?>"></iframe>
+        </p>
       </div>
 
       <div class="share">
@@ -57,16 +60,31 @@
         </section>
       </div>
 
+      <!-- Random posts -->
       <div class="more">
         <h2> Check out some other events! </h2>
+
+        <?php
+          $events_posts = get_posts([
+            'post_type' => 'event',
+            'numberposts' => 2,
+            'orderby' => rand
+          ]);
+
+          foreach ($events_posts as $post){
+            setup_postdata( $post );
+        ?>
         <div class="moreItem">
-          <p class="moreImage"> Image here </p>
-          <p class="moreType"> film </p>
+          <p class="moreVideo"> <iframe class="video" src="<?php echo get_field("trailer_embed_link")?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> </iframe> </p>
+          <p class="moreType"> <?= get_field('genre') ?> </p>
           <button class="moreBuyNow"> Buy Now! </button>
-          <h3 class="moreTitle"> The name of the Event </h3>
-          <p class="moreDescription"> The description of the event and stuff goes in here </p>
-          <p class="moreRestrictions"> The restrictions of the event here </p>
+          <h3 class="moreTitle"> <?= get_the_title() ?> </h3>
+          <p class="moreDescription"> <?= wp_trim_words( get_the_content(), 40, null ) ?> <br> <br> <?= get_field(restrictions) ?></p>
         </div>
+        <?php
+          wp_reset_postdata();
+          }
+        ?>
       </div>
 
     <?php endwhile; ?>
