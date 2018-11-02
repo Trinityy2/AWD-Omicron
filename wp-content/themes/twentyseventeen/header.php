45370e79d87c71477ee1262c4e91f2d12c691079
Tomo <tomohito95@hotmail.com>
2018-11-02 17:05:46 +1100

    removed logo

--- wp-content/themes/twentyseventeen/header.php
+++ wp-content/themes/twentyseventeen/header.php
@@ -29,7 +29,6 @@
 <div id="page" class="site">
 	<header id="masthead" class="site-header" role="banner">
 		<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-		<img id="logo" src="<?php echo get_template_directory_uri() . '/assets/images/hraff-logo.png'; ?>" alt="HRAFF Logo" width="80" height="80">
 		<nav class="site-nav">
                     <ul id = "menu">
                     <a href="#"><li>ABOUT</li></a>
@@ -65,19 +64,6 @@
 
 
 	</header><!-- #masthead -->
-
-	<?php
-
-	/*
-	 * If a regular post or page, and not the front page, show the featured image.
-	 * Using get_queried_object_id() here since the $post global may not be set before a call to the_post().
-	 */
-	if ( ( is_single() || ( is_page() && ! twentyseventeen_is_frontpage() ) ) && has_post_thumbnail( get_queried_object_id() ) ) :
-		echo '<div class="single-featured-image-header">';
-		echo get_the_post_thumbnail( get_queried_object_id(), 'twentyseventeen-featured-image' );
-		echo '</div><!-- .single-featured-image-header -->';
-	endif;
-	?>
 
 	<div class="site-content-contain">
 		<div id="content" class="site-content">
