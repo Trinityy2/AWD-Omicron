f8b7b83c8fc4ad54a0b88359cfab8f5d4c6cf333
Tomo <tomohito95@hotmail.com>
2018-11-02 16:36:12 +1100

    Fixed the 404 page

--- wp-content/themes/twentyseventeen/404.php
+++ wp-content/themes/twentyseventeen/404.php
@@ -1,21 +1,26 @@
 
 <?php get_header(); ?>
 
+<style>
+	<?php include "allfonts.css"; ?>
+	<?php include "404.css"; ?>
+</style>
+
 <div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
 
-					<h1 class="page-title"><?php ( 'Oh no! We were not able to find the page you are looking for'); ?></h1>
+					<h1 class="page-title">
+						<?php ( 'Oh no! We were not able to find the page you are looking for'); ?>
+					</h1>
 				</header><!-- .page-header -->
 				<div class="page-content">
-					<img src="<?php echo get_template_directory_uri() . '/assets/images/sadpanda.png'; ?>">
-					<p><?php _e( 'It seems like nothing was found here. Would you like to try search instead?'); ?></p>
-
+					<a href = "<?php echo get_home_url('/'); ?>">
+						<img class = "panda" src="<?php echo get_template_directory_uri() . '/assets/images/sadpanda.png'; ?>">
+					</a>
+					<h1><?php _e( 'Looks like you and this panda got lost. Let\'s take this panda back home...'); ?></h1>
 				</div>
 			</section>
-		</main>
-	</div>
+
 </div>
